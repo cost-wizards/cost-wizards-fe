55b039a752511adf9850f0acc4fd4e3d60482e17
ajaydangol014 <ajaydangol014@gmail.com>
2024-04-06 22:13:49 +0545

    Add loading component and also in manage in account over view page

---
+++ src/components/Loading.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Flex, Spin } from 'antd';
+
+const Loading = () => {
+	return (
+		<Flex
+			align='center'
+			gap='middle'
+		>
+			<Spin
+				size='large'
+				colorPrimary='#038e43'
+			/>
+		</Flex>
+	);
+};
+
+export default Loading;
